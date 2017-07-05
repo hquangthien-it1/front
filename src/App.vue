5c12d3fbe3bd5c8041731abb987d8110481e6d1f
hquangthien-it1 <hquangthien.it1@gmail.com>
2017-07-06 02:09:23 +0700

    v1.00
    fresh project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container" id="app">
+    <navbar></navbar>
+    <login></login>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Login from './components/authentication/Login.vue'
+export default {
+  components: {
+      'navbar': Navbar,
+      'login': Login
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/authentication/Login.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <form class="form-signin">
+                <h2 class="form-signin-heading">Please sign in</h2>
+                <label for="inputEmail" class="sr-only">Email address</label>
+                <input type="email" id="inputEmail" class="form-control" v-model="email" placeholder="Email address" required autofocus>
+                <br>
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" v-model="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                    </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
+        </div>
+        <pre>
+            {{ $data }}
+        </pre>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                email: '',
+                password: ''
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
